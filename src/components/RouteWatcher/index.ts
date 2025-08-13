7d631c7734f3767a4410a6db4474c57f69f92f7c
CadeLawless <clawless05@gmail.com>
2025-08-13 13:04:34 -0500

    combined into one form and fixed leave tab issue

--- src/components/RouteWatcher/index.ts
+++ src/components/RouteWatcher/index.ts
@@ -5,12 +5,6 @@ function RouteWatcher() {
   const location = useLocation();
 
   useEffect(() => {
-    //console.log(location.pathname);
-
-    if (location.pathname !== "/game-boards/create") {
-      localStorage.removeItem("gameBoard");
-    }
-
     // Replace :id with a wildcard check if you want to match any ID
     if (!location.pathname.startsWith("/game-boards/") || !location.pathname.endsWith("/edit")) {
       localStorage.removeItem("gameData");
